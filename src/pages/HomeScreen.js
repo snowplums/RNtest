50ff3f8cfe8bf605d327bf12add32fae68269c87
Wayne Mei <waynemm14@gmail.com>
2023-01-02 13:22:36 -0800

    Added LessonPage

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -2,10 +2,15 @@ import React from "react";
 //import AsyncStorage from "@react-native-async-storage/async-storage";
 import { View, Text, Button, StyleSheet } from "react-native";
 
+import LessonPage from "./LessonPage";
+
 const HomeScreen = ({ navigation }) => {
+  const { nav } = this.props.navigation;
+
   return (
     <View>
       <Text>Home Screen</Text>
+      <Button title="View Lessons" onPress={() => navigate("LessonPage")} />
     </View>
   );
 };
---
+++ src/pages/LessonPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const LessonPage = () => {
+  return (
+    <View>
+      <Text>Lessons: </Text>
+    </View>
+  );
+};
+
+export default LessonPage;
