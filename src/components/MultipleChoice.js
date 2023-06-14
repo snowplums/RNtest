af012062184537aee643a51d69c378654c683dc7
Wayne Mei <waynemm14@gmail.com>
2023-06-13 18:38:14 -0700

    Created Multiple Choice Template

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -3,9 +3,27 @@ import React from "react";
 import { View, Text, Image, Button, StyleSheet, Pressable } from "react-native";
 
 const MultipleChoice = (props) => {
+
+  const checkAnswer = (choice) => {
+    if (choice === props.answer) {
+      console.log("Right Answer");
+    } else {
+      console.log("Wrong Answer");
+    }
+  };
+
   return (
     <View>
       <Text>Multiple Choice</Text>
+      {
+        props.choices.map((choice) => {
+        return(
+          <Button title={choice} onPress={checkAnswer.bind(this, choice)} key={choice}/>
+        )
+        })
+      }
+        
+      
     </View>
   );
 };
